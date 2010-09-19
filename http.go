bcb72d752e244708f69a39603c39415b0ae14dd2
Ian Melven <ian.melven@gmail.com>
2010-09-19 01:03:35 -0700

    milestone 3 without .. in the listing and all that entails

--- http.go
+++ http.go
@@ -1,5 +1,9 @@
 // random info : go has some level of unit testing support in the 'testing' package
 
+// i kept on forgetting to use := for initializer syntax
+// it doesn't imply anything about the type it seems, it just means
+// you can leave off 'var' at the beginning :/
+
 package main
 
 import (
@@ -7,6 +11,7 @@ import (
   "flag" // command line option parser
   "fmt" // printf
   "net" // network
+  "strings" // split
 )
 
 const (
@@ -31,7 +36,7 @@ func main() {
   // exported/public functions in packages start with capital letters !
   flag.Parse() // parse command line arguments if any
   
-  // special initialization syntax that implicitly types variables !
+  // special initialization syntax that just means we can leave var off the declaration :( 
   // func Getwd() (string, Error)
   rootdir, err := os.Getwd()
 
@@ -45,6 +50,8 @@ func main() {
   fmt.Printf("\nstarting up on port %d\n", *port)
   fmt.Printf("\nusing %s as root directory for serving\n\n", rootdir)
 
+  root_url := fmt.Sprintf("http://127.0.0.1:%d", *port)
+
   addr := net.TCPAddr{net.ParseIP("0.0.0.0"), *port}
 
   // bind to address/port, l is a TCPListener 
@@ -53,6 +60,10 @@ func main() {
   if err != nil {
     fatal("could not listen !, error = " + err.String())
   }
+
+  var req_full_path string = ""
+  var base_url string = ""
+  //var current_path string = ""
 
   // wait for connections in infinite loop using weird for syntax 
   for {
@@ -65,24 +76,62 @@ func main() {
 
     fmt.Printf("accepted connection from %s\n", conn.RemoteAddr())
 
-    // this was milestone 1
-    //content := "<html><body>HELLO WORLD!</body></html>"
+    // milestone 3 code
 
-    // milestone 2
-    info, err := os.Lstat(rootdir)
+    // func (c *TCPConn) Read(b []byte) (n int, err os.Error)
+
+    // this is a SLICE so it is created with make
+    request := make([]byte, 2048)
+
+    n, err := conn.Read(request)
+
+    if (err != nil || n == 0) {
+      fatal("could not read from tcp connection, error = " + err.String())
+    }
+
+    req_string := string(request)
+
+    fmt.Printf("got request from connection : %s", req_string)    
+
+    // -1 means 'return all substrings' - feels kinda win32 api-ish :/
+    splits := strings.Split(req_string, " ", -1)
+
+    req_path := splits[1]
+
+    fmt.Printf("req_path is %s\n", req_path)
+
+    // TODO: make sure .. is in the listing
+    // TODO: avoid directory traversal above rootdir
+    // TODO: check if the request is a directory or is a regular file
+    //   and handle accordingly 
+
+    if req_path == "/" {
+      req_full_path = rootdir + "/"
+      base_url = root_url
+    } else {
+      req_full_path = rootdir + req_path
+
+      base_url = root_url + req_path
+    }
+
+    fmt.Printf("base_url is %s\n", base_url)
+
+    fmt.Printf("req_full_path is %s\n", req_full_path)
+
+    info, err := os.Lstat(req_full_path)
 
     if (err != nil) {
-      fatal("couldn't stat directory, error = " + err.String())
+      fatal("couldn't stat directory " + req_full_path + ", error = " + err.String())
     }
 
     if (info.IsDirectory() == false) {
       fatal("the directory isn't a directory ?!??!")
     }
 
-    directory, err := os.Open(rootdir, 0, 0)
+    directory, err := os.Open(req_full_path, 0, 0)
 
     if (err != nil) {
-      fatal("couldn't open directory listing for " + rootdir + " , error = " + err.String())
+      fatal("couldn't open directory listing for " + req_full_path + " , error = " + err.String())
     }
 
     // dir_listing is an array of FileInfo structures in 'directory order'
@@ -96,12 +145,13 @@ func main() {
     content := "<html><body>"
 
     for i := 0; i < len(dir_listing); i++ {
-      content += dir_listing[i].Name + "<p>"
+      dir_entry := dir_listing[i].Name
+      content += "<a href = " + base_url + "/" + dir_entry + ">" + dir_entry + "</a><p>"
     }
 
     content += "</body></html>"
 
-    // END of milestone 2 code
+    // END of milestone 3 code 
 
     headers := "Content-Type: text/html; charset=UTF-8" + terminator
 
@@ -113,8 +163,9 @@ func main() {
 
     response := ok + headers + content + terminator + terminator
 
-    // casting magic !
-    n, err := conn.Write([]byte(response))
+    // type-casting magic (sort of)!
+    // also note no := here because both of these have been := before.. 
+    n, err = conn.Write([]byte(response))
 
     if (err != nil || n == 0) {
       fatal("could not write to tcp connection, error = " + err.String())
