97321f4467140a69a5457cb761e68aadf323381d
Ian Melven <ian.melven@gmail.com>
2010-09-18 18:45:39 -0700

    milestone 1 !

--- http.go
+++ http.go
@@ -1,3 +1,5 @@
+// random info : go has some level of unit testing support in the 'testing' package
+
 package main
 
 import (
@@ -7,13 +9,21 @@ import (
   "net" // network
 )
 
-const defaultPort = 6667
+const (
+  defaultPort = 6667
+  terminator = "\r\n"
+  ok = "HTTP/1.1 200 OK" + terminator
+)
 
 // set up command line options we take
 var port = flag.Int("p", defaultPort, "TCP port to serve on")
 
 // TODO : concurrency with goroutines
-// TODO : fix all TODOs
+
+func fatal(msg string) {
+  fmt.Printf("%s\n\n", msg)
+  os.Exit(1)
+}
 
 // compiler says : opening braces MUST be on the same line !!!! >:o
 func main() {
@@ -28,28 +38,51 @@ func main() {
   // have to explicitly check != nil - compiler won't let you do if err because err
   // is not a boolean !
   if err != nil {
-    fmt.Printf("Could not get current working dir, error was %s\n", err.String())
+    fatal("Could not get current working dir, error = " +  err.String())
   }
 
   // have to dereference port here !!
   fmt.Printf("\nstarting up on port %d\n", *port)
   fmt.Printf("\nusing %s as root directory for serving\n\n", rootDir)
 
-  // bind to port 
+  addr := net.TCPAddr{net.ParseIP("0.0.0.0"), *port}
 
-  // l is a TCPListener 
-  l, err := ListenTCP(net string, laddr *TCPAddr)
+  // bind to address/port, l is a TCPListener 
+  l, err := net.ListenTCP("tcp4", &addr)
 
   if err != nil {
+    fatal("could not listen !, error = " + err.String())
+  }
 
+  // wait for connections in infinite loop using weird for syntax 
+  for {
+    // func (l *TCPListener) AcceptTCP() (c *TCPConn, err os.Error)
+    conn, err := l.AcceptTCP()
 
-  // wait for connections in infinite loop
+    if (err != nil) {
+      fatal("could not accept tcp connection !, error = " + err.String())
+    }
 
-//  for (;;) {
+    fmt.Printf("accepted connection from %s\n", conn.RemoteAddr())
 
-//  }
+    content := "<html><body>HELLO WORLD!</body></html>"
+
+    headers := "Content-Type: text/html; charset=UTF-8" + terminator
+    headers += fmt.Sprintf("Content-Length: %d", len(content)) + terminator
+    headers += terminator
+
+    response := ok + headers + content + terminator + terminator
+
+    // casting magic !
+    n, err := conn.Write([]byte(response))
+
+    if (err != nil || n == 0) {
+      fatal("could not write to tcp connection, error = " + err.String())
+    }
+
+    conn.Close()
+  }
 
 //  func Chdir(dir string) Error
 
-  os.Exit(0)
 }
